58486fcfdf7c6b6a1f3f69030a819cb0f795d4ca
Granc3k <mart.simon17@gmail.com>
2023-05-25 07:44:37 +0200

    muzika Sadge

--- 2223alg2-semestralproject-Granc3k/reservationSystemTenis/src/main/java/granc3k/semestralproject/reservationsystemtenis/Main.java
+++ 2223alg2-semestralproject-Granc3k/reservationSystemTenis/src/main/java/granc3k/semestralproject/reservationsystemtenis/Main.java
@@ -1,6 +1,5 @@
 package granc3k.semestralproject.reservationsystemtenis;
 
-import granc3k.semestralproject.reservationsystemtenis.utils.Music;
 
 import java.io.*;
 import java.time.LocalDateTime;
@@ -24,13 +23,6 @@ public class Main {
         boolean switchedWeeks = false;
         String prompt = "current_"+thisWeek;
         boolean debug = false;
-
-        //music to background
-        System.out.println("Chcete pustit hudbičku ke kódu?? a/n");
-        String dec = sc.next();
-        if(dec.equals("a")) {
-            Music.play("npc_move.wav", 1F);
-        }
 
         //used for switching between codes
         Reservations reservations = reservations_current;
